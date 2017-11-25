66ea2720eeb5fb7655569b0ee1ceba8b2f0c5ca0
Dunia Alrabadi <mac@Macs-MacBook-Pro.local>
2017-11-25 11:19:23 -0800

    Editted the fucntions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def index
+      @pages = Note.all
   end
 
   def show
+    @page = Note.find(params[:id])
   end
 
   def new
+    @page = Note.new
   end
 end
